87ea6d3de4bc0e69513f9bc38405aedbe7847893
flydev <code@sekretservices.com>
2023-05-29 16:06:59 +0200

    added: Hook after AdminThemeUikit::renderFile

--- AdminStyleDark.module.php
+++ AdminStyleDark.module.php
@@ -25,7 +25,33 @@ class AdminStyleDark extends WireData implements Module {
     ];
   }
 
+  // module init
+  public function init() {
+    $this->addHookAfter('AdminThemeUikit::renderFile', $this, 'hookRenderFile');
+  }
+
   public function ready() {
     $this->loadStyle(__DIR__ . "/style/dark.less");
+  }
+
+  /**
+   * Hook after AdminThemeUikit::renderFile
+   * Replace {{OFFCANVAS_LOGO}} with the logo URL if set in AdminThemeUikit's settings
+   * Note: Only works if `site/templates/AdminThemeUikit/_offcanvas.php` exists.
+   * @param HookEvent $event
+   */
+  function hookRenderFile($event) {
+    $file = $event->arguments(0); // full path/file being rendered 
+    if(basename($file) === '_offcanvas.php') {
+      $logoURL = $event->object->logoURL;
+  
+      $markup = str_replace(
+        "{{OFFCANVAS_LOGO}}",
+        $logoURL,
+        $event->return
+      );
+
+      $event->return = $markup;
+    }
   }
 }
